06c432c03ebca6fcf5311407877f9c831463cb21
vitycola <v.valero22@gmail.com>
2018-07-04 18:38:26 +0200

    first commit

--- datos.py
+++ datos.py
@@ -1,12 +1,27 @@
 import requests
-from lxml import etree as ET
-import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ElementTree as etree
 
-parser = ET.XMLParser(encoding='utf-8')
+# r1 = requests.get("https://data.tfl.gov.uk/tfl/syndication/feeds/stations-facilities.xml?app_id=3a0c4a01&app_key=ea95c6674605181c5dde64c7bb5d883c")
+# r2 = requests.get("https://tfl.gov.uk/tfl/syndication/feeds/step-free-tube-guide.xml")
+# #print (r1.text)
+# #r1.encoding = 'utf-8'
+# with open('stations-facilities.xml', 'wb') as f:
+#    f.write(r1.content)
+# with open('step-free-tube-guide.xml', 'wb') as f:
+#    f.write(r2.content)
 
-#stations = ET.parse('datos/stations-facilities.xml',parser)
-tube = ET.parse('datos/step-free-tube-guide.xml',parser)
+tree = etree()
+tree.parse("step-free-tube-guide.xml")
+root = tree.getroot()
+for child in root.findall('./'):
+    print (child.tag, child.attrib)
+#
+# i = 0
+# for stat in root[7].findall('station'):
+#     borrar = stat.find('openingHours')
+#     print(borrar)
+# #    root[7].remove(borrar)
+#    for child in root[7][i].iter():
+#        print(stat.tag, stat.attrib)
+#
 
-#root = stations.getroot()
-
-#print(root)
--- getdata.py
+++ getdata.py
@@ -1,13 +1,10 @@
 import requests
 
-#r1 = requests.get("https://data.tfl.gov.uk/tfl/syndication/feeds/stations-facilities.xml?app_id=3a0c4a01&app_key=ea95c6674605181c5dde64c7bb5d883c")
+
+r1 = requests.get("https://data.tfl.gov.uk/tfl/syndication/feeds/stations-facilities.xml?app_id=3a0c4a01&app_key=ea95c6674605181c5dde64c7bb5d883c")
 r2 = requests.get("https://tfl.gov.uk/tfl/syndication/feeds/step-free-tube-guide.xml")
 
-r2.encoding = 'utf-8'
-
-
-#with open('datos/stations-facilities.xml', 'w') as f:
-#    f.write(r1.text)
-
-with open('datos/step-free-tube-guide.xml', 'wb') as f:
+with open('stations-facilities.xml', 'wb') as f:
+   f.write(r1.content)
+with open('step-free-tube-guide.xml', 'wb') as f:
    f.write(r2.content)
